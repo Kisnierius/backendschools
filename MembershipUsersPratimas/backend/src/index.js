cebf2fe50719cdfb8f2f5990958de9a526194937
Kisnierius <84340591+Kisnierius@users.noreply.github.com>
2021-09-04 13:55:56 +0300

    Back-end part for user management excercise
    MongoDB database with 2 collections:
    - users
    - memberships
    
    Functions applied:
    - post memberships;
    - get memberships from database
    - delete memberships
    
    - post users, with data validation
    - get users with agregation, lookup from another colletction, sorting
    
    Back-end part

--- MembershipUsersPratimas/backend/src/index.js
+++ MembershipUsersPratimas/backend/src/index.js
@@ -20,7 +20,6 @@ app.get('/', async (request, response) => {
     await client.connect();
     const database = client.db('membershipmanagement');
     const collection = database.collection('memberships');
-
     const memberships = await collection.find().sort('name', request.query.sort?.toLowerCase() === 'dsc' ? -1 : 1).toArray();
     await client.close();
     return response.status(200).json(memberships);
@@ -36,17 +35,12 @@ app.post('/', async (request, response) => {
     if (typeof name !== 'string') {
       errors.push('name must be a string');
     }
-    // if (!['cat', 'dog', 'bunny'].includes(type)) {
-    //   errors.push('type must be cat, dog or bunny');
-    // }
-
     if (!Number.isInteger(price) || price < 0 || price > 99) {
       errors.push('price be an integer between 1 and 99');
     }
     if (errors.length > 0) {
       return response.status(400).json({ errors });
     }
-
     await client.connect();
     const database = client.db('membershipmanagement');
     const collection = database.collection('memberships');
@@ -72,21 +66,17 @@ app.delete('/:membershipId', async (request, response) => {
   }
 });
 
-
-
 // POSTINAM USERIUS
 
 app.post('/users', async (request, response) => {
   try {
-    const { name, surname, email, serviceId } = request.body;
+    const {
+      name, surname, email, serviceId,
+    } = request.body;
     const errors = [];
     if (typeof name !== 'string') {
       errors.push('name must be a string');
     }
-    // if (!['cat', 'dog', 'bunny'].includes(type)) {
-    //   errors.push('type must be cat, dog or bunny');
-    // }
-
     if (typeof surname !== 'string') {
       errors.push('surname must be a string');
     }
@@ -96,26 +86,23 @@ app.post('/users', async (request, response) => {
     if (typeof serviceId !== 'string') {
       errors.push('service_id must be a string');
     }
-  
     await client.connect();
     const database = client.db('membershipmanagement');
     const membershipsCollection = database.collection('memberships');
     const membership = await membershipsCollection.findOne({ _id: ObjectId(serviceId) });
- 
-    console.log("3est");
     await client.close();
-    console.log("4est");
-
-    if ( !membership) {
-        errors.push('membership ID not found');
+    if (!membership) {
+      errors.push('membership ID not found');
     }
     if (errors.length > 0) {
-      console.log(errors)
+      console.log(errors);
       return response.status(400).json({ errors });
     }
     await client.connect();
     const userscollection = database.collection('users');
-    const users = await userscollection.insertOne({ name: name.trim(), surname: surname.trim(), email, service_id: serviceId });
+    const users = await userscollection.insertOne({
+      name: name.trim(), surname: surname.trim(), email, service_id: ObjectId(serviceId),
+    });
     await client.close();
     return response.status(200).json(users);
   } catch (error) {
@@ -124,15 +111,44 @@ app.post('/users', async (request, response) => {
 });
 
 /// getinam userius
+//* ****** get funkcija su membership name paemimu is kitos kolekcijos ir sort by name
 
-app.get('/users', async (request, response) => {
+app.get('/users/:order(asc|dsc)', async (request, response) => {
   try {
+    const { order } = request.params;
+    console.log(request.params);
+    console.log(order);
     await client.connect();
     const database = client.db('membershipmanagement');
     const collection = database.collection('users');
-// agregation stages find, sort, susirasti pagal id memberships kolekcijoj name
-// stages: match, lookup, sort, project.
-    const users = await collection.//find().sort('name', request.query.sort?.// trinam iki cia: toLowerCase() === 'dsc' ? -1 : 1).toArray();
+    const users = await collection.aggregate([
+      {
+        $match: {},
+      },
+      {
+        $lookup:
+        {
+          from: 'memberships',
+          localField: 'service_id',
+          foreignField: '_id',
+          as: 'service',
+        },
+      },
+      {
+        $unwind: '$service',
+      },
+      {
+        $project: {
+          name: 1, surname: 1, email: 1, membershipName: '$service.name',
+        },
+      },
+      {
+        $sort: { name: order === 'dsc' ? -1 : 1 },
+      },
+    ]).toArray();
+
+    console.log(users);
+
     await client.close();
     return response.status(200).json(users);
   } catch (error) {
@@ -140,24 +156,8 @@ app.get('/users', async (request, response) => {
   }
 });
 
-
-
-
 app.listen(PORT, () => {
   console.log(
     `My app is running and listening to port http://localhost:${PORT}/`,
   );
 });
-
-/// app.get('/', async (request, response) => {
-///    try {
-///      await client.connect();
-///      const database = client.db('PetsDB');
-///      const collection = database.collection('pets');
-///      const pets = await collection.find().toArray();
-///      await client.close();
-///      return response.status(200).json(pets);
-///    } catch (error) {
-///      return response.status(500).json({ error });
-///    }
-///  });
